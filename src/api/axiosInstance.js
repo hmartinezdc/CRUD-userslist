2eb4e642c349c1b2c9a0a0094c6a25844ad96d4c
Hember Martinez <andrehp.9308@gmail.com>
2023-08-06 15:14:21 -0500

    chage baseurl

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://users-crud-c8z1.onrender.com/",
+  baseURL: "https://users-crud-api-mytf-dev.fl0.io//",
 });
 
 // baseURL: "https://users-crud.academlo.tech/",
